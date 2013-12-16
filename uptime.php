1ddbc6132a4846fa717a0905180ba931e50db460
psycholyzern <firdaus696@gmail.com>
2013-12-16 10:51:14 +0800

    Update uptime.php
    change some code

--- uptime.php
+++ uptime.php
@@ -35,19 +35,21 @@ fclose($fh);
 $memmath = $memcache + $memfree;
 $memmath2 = $memmath / $memtotal * 100;
 $memory = round($memmath2) . '%';
+$memoryc = round($memmath/1024) . "MB/" . round($memtotal/1024) . "MB";
 
 if ($memory >= "51%") { $memlevel = "success"; }
 elseif ($memory <= "50%") { $memlevel = "warning"; }
 elseif ($memory <= "35%") { $memlevel = "danger"; }
 
 $array['memory'] = '<div class="progress progress-striped active">
-<div class="bar bar-'.$memlevel.'" style="width: '.$memory.';">'.$memory.'</div>
+<div class="bar bar-'.$memlevel.'" style="width: '.$memory.';">'.$memoryc.'</div>
 </div>';
 
 $hddtotal = disk_total_space("/");
 $hddfree = disk_free_space("/");
 $hddmath = $hddfree / $hddtotal * 100;
 $hdd = round($hddmath) . '%';
+$hddc = round($hddfree/1024 / 1024 / 1024) . "GB/" . round($hddtotal/1024 / 1024 / 1024) . "GB";
 
 if ($hdd >= "51%") { $hddlevel = "success"; }
 elseif ($hdd <= "50%") { $hddlevel = "warning"; }
@@ -55,7 +57,7 @@ elseif ($hdd <= "35%") { $hddlevel = "danger"; }
 
 
 $array['hdd'] = '<div class="progress progress-striped active">
-<div class="bar bar-'.$hddlevel.'" style="width: '.$hdd.';">'.$hdd.'</div>
+<div class="bar bar-'.$hddlevel.'" style="width: '.$hdd.';">'.$hddc.'</div>
 </div>';
 
 $load = sys_getloadavg();
